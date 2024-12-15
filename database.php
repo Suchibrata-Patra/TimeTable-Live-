91baffdb24eb62ac855115c236a0c12a9d7840b4
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-15 12:29:52 +0530

    Update database.php

--- database.php
+++ database.php
@@ -27,14 +27,6 @@ $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
-}
-
 if (empty($host) || empty($user) || empty($password) || empty($dbname)) {
     error_log("Missing or incorrect environment variables.", 0);
     die("An error occurred while configuring the database connection.");
@@ -49,16 +41,17 @@ if (!preg_match('/^[a-zA-Z0-9_]+$/', $dbname)) {
     error_log("Invalid DB_NAME value.", 0);
     die("Invalid database name.");
 }
-
+mysqli_report(MYSQLI_REPORT_STRICT);
 try {
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-    $conn = new PDO($dsn, $user, $password);
+    $conn = new mysqli($host, $user, $password, $dbname);
     
-    // Set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    echo "Successfully connected!";
-} catch (PDOException $e) {
+    // Check the connection
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Successfully connected!";
+    }
+} catch (Exception $e) {
     // Log the error securely without exposing sensitive details
     error_log($e->getMessage(), 0);
     die("Unable to connect to the database.");
