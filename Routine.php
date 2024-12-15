b43647009077db518bde70e47c86eb14bc41230a
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2024-12-15 12:06:21 +0530

    Update Routine.php

--- Routine.php
+++ Routine.php
@@ -1,4 +1,7 @@
 <?php include 'database.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Fetch all teachers
 $teachersQuery = $conn->query("SELECT Teacher_ID, Teacher_Name FROM teacher_profile");
 $teachers = $teachersQuery->fetchAll(PDO::FETCH_ASSOC);
