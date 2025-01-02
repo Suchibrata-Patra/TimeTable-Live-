176a766366634768d271600bf3e5f4f037b3cb5e
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-02 17:15:35 +0530

    Update scheduler.php

--- scheduler.php
+++ scheduler.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="Asset/CSS/popup.css">
   <script src="Asset/JS/fetcher.js"></script>
   <script src="Asset/JS/scheduler.js"></script>
-  <script src="Asset/JS/date_detector.js"></script>
+  <!-- <script src="Asset/JS/date_detector.js"></script> -->
 
   <script>
     document.addEventListener('DOMContentLoaded', () => {
