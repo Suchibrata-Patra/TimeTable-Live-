9dd011ab03d20e8f86237b6606d0623c9b41a3cc
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-02 18:45:33 +0530

    Update scheduler.php

--- scheduler.php
+++ scheduler.php
@@ -8,9 +8,9 @@
   <link rel="icon" href="favicon.png">
   <link rel="stylesheet" href="Asset/CSS/style.css">
   <link rel="stylesheet" href="Asset/CSS/popup.css">
-  <script src="Asset/JS/fetcher.js"></script>
-  <script src="Asset/JS/scheduler.js"></script>
-  <script src="Asset/JS/date_detector.js"></script>
+  <script src="/Asset/JS/fetcher.js"></script>
+  <script src="/Asset/JS/scheduler.js"></script>
+  <script src="/Asset/JS/date_detector.js"></script>
 
   <script>
     document.addEventListener('DOMContentLoaded', () => {
@@ -286,7 +286,7 @@
       </span>
     </div>
 
-    <link rel="stylesheet" href="Asset/CSS/animation.css">
+    <link rel="stylesheet" href="/Asset/CSS/animation.css">
   </div>
   <script>
     window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };
