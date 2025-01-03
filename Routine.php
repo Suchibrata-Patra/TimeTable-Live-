c5ef92b52274448231c153c7791dc4d48359cefc
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-03 21:05:10 +0530

    Update Routine.php

--- Routine.php
+++ Routine.php
@@ -7,7 +7,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$refresh_query = "DELETE FROM class_schedule WHERE CLASS = 'Delete'";
+$refresh_query = "DELETE FROM class_schedule WHERE CLASS = 'Delete' OR Class_Time = 'Delete'";
 $conn->query($refresh_query);
 
 
